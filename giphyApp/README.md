f0329b80228caa972bfdb949cf42ec6b1219bded
Kyle Thomas <kthomas852@yahoo.com>
2018-09-08 09:50:57 -0700

    progress with giphy

---
+++ giphyApp/README.md
@@ -0,0 +1,13 @@
+Giphy Image App
+Written by Kyle Thomas 2018
+
+Welcome the the readme for the Giphy image App.  
+
+This app is designed to shortcut the process for finding gif images.  Simply enter the
+type of gif you would like to find.  A speacial button to help locate the gif will be 
+created for you.
+
+Clicking on the different buttons will propigate gif images using the terms you have
+already provided.
+
+Enjoy!
--- giphyApp/assets/javascript/giphy.js
+++ giphyApp/assets/javascript/giphy.js
@@ -1,31 +1,69 @@
-$('document').ready(function(){
-$("#cat-button").click(function() {
+var gifs = ["cats", "dogs"];
 
-    //
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=motivational";
-    console.log('Cats a coming!');
-    //
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
+function renderButtons(){
+  $('#button-area').html('');
+  for (let i = 0; i < gifs.length; i++) {
+    let element = $("<button>");
+    element.addClass("selector");
+    element.attr("data-name", gifs[i]);
+    element.text(gifs[i]);
+    $('#button-area').append(element);
+  }
+  clickButtons();
+  console.log(gifs);
+};
+
+//Creates Gif based on button type selected
+function clickButtons(){
+$('.selector').click(function(){
+  console.log('click');
+  let gifTag = $(this).attr('data-name');
+  console.log(gifTag);
+  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + gifTag;
+  console.log('Cats a coming!');
+  //AJAX query to get GIF images
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    var imageUrl = response.data.image_original_url;
+    var gifImage = $("<img>");
+    gifImage.addClass('gif');
+    gifImage.attr("src", imageUrl);
+    gifImage.attr("alt", "Gif image");
     
     //
-    .then(function(response) {
-      
-      //
-      //console.log(response);
-        var imageUrl = response.data.image_original_url;
-
-        //
-        var catImage = $("<img>");
-
-        //
-        catImage.attr("src", imageUrl);
-        catImage.attr("alt", "cat image");
-
-        //
-        $("#images").html(catImage);
-      });
+    $("#images").append(gifImage);
+    clickGifs();
   });
+});
+};
+
+//Activates image Gif based on status
+function clickGifs(){
+  $('.gif').click(function(){
+    let state = $(this).attr();
+    let animate = $(this).attr();
+    let still = $(this).attr();
+    if(state === 'still'){
+      state = 'animate';
+      $(this).attr(animate);
+    }else if(state === 'animate'){
+      state = 'still';
+      $(this).attr(still);
+    }
+  });
+};
+
+//Main listening events for buttons and input
+$('document').ready(function(){
+  console.log('Ready for Action!');
+$('#add-gif').click(function(){
+  let gTemp = $('#gif-input').val().trim();
+  gifs.push(gTemp);
+  renderButtons();
+});
+
+console.log('Done');
 });
