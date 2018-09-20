d370310ea21fa89b865bf43ca88bfe2c0f2b1822
Kyle Thomas <kthomas852@yahoo.com>
2018-09-19 19:22:37 -0700

    Finished RPS project

--- RPS-mutiplayer/assets/javascript/app.js
+++ RPS-mutiplayer/assets/javascript/app.js
@@ -33,8 +33,8 @@
               player1Points: p1,
               player2Points: p2,
               win: false,
-              chatPlayer1: ['Player1 Ready'],
-              chatPlayer2: ['Player2 Ready']
+              chatPlayer1: ['.'],
+              chatPlayer2: ['.']
           });
           me = 'player1';
       } else if(mem.player2 === ''){
@@ -140,14 +140,26 @@
     $('#playerSpace2').attr('src', picture2);
   };
 
-  var chatUpdate = function(array, player){
-    for(i=0; array.length > i; i++){
-    let chatPlay = $('<strong>').text(player + ':');
-    let chatter = $('<p>').text(array[i]);
-    let contain = $('<div class="container">');
-    $('#chat').append(contain);
-    $('#chat').append(chatPlay);
-    $('#chat').append(chatter);
+  var chatUpdate = function(array, player, i){
+      if (array[i] === '' || array[i] === '.') {
+          console.log('skip line');
+      } else {
+          let chatPlay = $('<strong>').text(player + ':');
+          let chatter = $('<p>').text(array[i]);
+          let contain = $('<div class="container">');
+          $('#chat').append(contain);
+          $('#chat').append(chatPlay);
+          $('#chat').append(chatter);
+      }
+  };
+
+  var scoreUpdate = function(){
+    if(userName === mem.player1){
+        $('#win').text('Wins: ' + mem.player1Points);
+        $('#loss').text('Losses: ' + mem.player2Points);
+    }else if(userName === mem.player2){
+        $('#win').text('Wins: ' + mem.player2Points);
+        $('#loss').text('Losses: ' + mem.player1Points);
     }
   };
 
@@ -156,12 +168,12 @@
         console.log('Player 1 Wins!!!!');
         $('#banner').text(mem.player1 + ' Wins!!!!');
         rps.ref('/win').set(true);
-        //resetOption();
+        $('.end').attr('style', 'visibility: visible');
     } else if(mem.player2Points === 3){
         console.log('Player 2 Wins!!!!');
         $('#banner').text(mem.player2 + ' Wins!!!!');
         rps.ref('/win').set(true);
-        //resetOption();
+        $('.end').attr('style', 'visibility: visible');
     } else{
         console.log('Next Round');
         setTimeout(() => {
@@ -201,34 +213,45 @@
       };
     });
     //Firebase changes listener
-    rps.ref().on('value', function(snapshot){
-        mem = snapshot.val();
-        console.log(mem);
-        compareGuess();
-        chatUpdate(mem.chatPlayer1, mem.player1);
-        chatUpdate(mem.chatPlayer2, mem.player2);
-    });
+rps.ref().on('value', function (snapshot) {
+    mem = snapshot.val();
+    console.log(mem);
+    compareGuess();
+    $('#chat').html('');
+    for (i = 0; mem.chatPlayer1.length > i; i++) {
+        chatUpdate(mem.chatPlayer1, mem.player1, i);
+        chatUpdate(mem.chatPlayer2, mem.player2, i);
+    }
+    scoreUpdate();
+});
 
     $('.btn-sm').click(function(){
         let post = $('#chatter').val();
         let chatArray = [];
+        let chatArrayOther = [];
         if(userName === mem.player1){
             chatArray = mem.chatPlayer1;
+            chatArrayOther = mem.chatPlayer2;
             console.log(chatArray);
             chatArray.push(post);
+            chatArrayOther.push('');
             console.log(post);
             rps.ref('/chatPlayer1').set(chatArray);
+            rps.ref('/chatPlayer2').set(chatArrayOther);
         }else if(userName === mem.player2){
             chatArray = mem.chatPlayer2;
+            chatArrayOther = mem.chatPlayer1;
             console.log(chatArray);
             chatArray.push(post);
+            chatArrayOther.push('');
             console.log(post);
             rps.ref('/chatPlayer2').set(chatArray);
+            rps.ref('/chatPlayer1').set(chatArrayOther);
         }
         $('#chatter').val('');
     });
     //Reset Listener
-    /*$('#reset').click(function(){
+    $('.end').click(function(){
         console.log('Reset');
         location.reload();
-    });*/
+    });
